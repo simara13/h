72570d3b02f280397d83c54152ca1ac7f5457b92
simara13 <167622606+simara13@users.noreply.github.com>
2024-04-20 16:17:15 +0400

    Add files via upload

---
+++ h.js
@@ -0,0 +1,64 @@
+// var a=10;
+// let b=20;
+// const c=30;
+// console.log(a, b, c);
+// document.getElementById("demo").innerHTML=a + b + c;
+// let a=789;
+// let b=-56;
+// let c=0;
+// console.log(a, b, c);
+// document.getElementById("demo").innerHTML=a + b + c;
+// let a= 23;
+// let b= 45;
+// let c= 67;
+// console.log(a, b, c);
+// document.getElementById("demo").innerHTML=a * b * c;
+// const a="Azer";
+// const b="gezintiye";
+// const c="cixdi."
+// console.log(a, b, c);
+// document.getElementById("demo").innerHTML=a + " " + b + " " + c;
+// var a="seher";
+// var b="hava";
+// var c="isti";
+// var d="idi.";
+// console.log(a, b, c);
+// document.getElementById("demo").innerHTML=a + " " + b + " " + c + " " + d;
+// function myfun(a,b) {
+//    return a*b;
+// }
+// document.getElementById("demo").innerHTML=myfun(5, 6);
+// function myfun1(a,b) {
+//    return a-b;
+// }
+// document.getElementById("demo1").innerHTML=myfun1(5, 6);
+// function myfun2(a,b) {
+//    return a+b;
+// }
+// document.getElementById("demo2").innerHTML=myfun2(5, 6);
+// function myfun3(a,b) {
+//    return a/b;
+// }
+// document.getElementById("demo3").innerHTML=myfun3(5, 6);
+// const cars = { color: "red", model: "Mustang", year: 1964 };
+// document.getElementById("demo").innerHTML = typeof cars;
+// let abid = 18;
+// let osman = 19;
+// if (abid > osman) {
+//     document.getElementById("demo").innerHTML = abid;
+// } else {
+//     document.getElementById("demo").innerHTML = osman;
+// }
+// const day = new Date().getDay();
+// document.getElementById("demo").innerHTML = day;
+// class Person {
+//     constructor(fname, lname) {
+//         this.firstname = fname;
+//         this.lastname = lname;
+//     }
+//     hello() {
+//         return "Hello " + this.firstname + " " + this.lastname;
+//     }
+// }
+// let p = new Person("John", "Doe");
+// document.getElementById("demo").innerHTML = p.hello();
